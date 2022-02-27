91a02ee3b3c808a9d9dc4a380512a71197566e09
Brooke Kennison <brookelkennison@gmail.com>
2022-02-26 22:23:24 -0500

    add iframe for video

--- src/components/VideoPreview.js
+++ src/components/VideoPreview.js
@@ -2,6 +2,11 @@ import React, { useEffect, useState } from "react";
 
 function VideoPreview(props) {
 
+    // Improvements
+        // Only render a video on Click
+
+
+
     const [covidVideos, setCovidVideos] = useState([]);
     const [nonCovidVideos, setNonCovidVideos] = useState([]);
     // move this to parent
@@ -13,17 +18,16 @@ function VideoPreview(props) {
 
     const list = [];
 
-    // fetch all videos
-
     // videos with COVID-19 in the title will go in a serparate array
     function fetchVideos(){
         // Where we're fetching data from
-        fetch("https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyDXGwBHzog56FMxQyusEwWXyp67ZZYqDHo&part=snippet&channelId=UCL03ygcTgIbe36o2Z7sReuQ&maxResults=50").then(response => response.json())
+        fetch("https://youtube.googleapis.com/youtube/v3/search?key=AIzaSyD-y9MrdrHxyCUpNuqsrPYKfWSdYw2zo-E&part=snippet&channelId=UCL03ygcTgIbe36o2Z7sReuQ&maxResults=10").then(response => response.json())
         .then((data) => {
             setNonCovidVideos(data.items);
+            console.log(data.items)
         });
 
-        fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?key=AIzaSyDXGwBHzog56FMxQyusEwWXyp67ZZYqDHo&playlistId=PLogA9DP2_vSekxHP73PXaKD6nbOK56CJw&o&part=snippet")
+        fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?key=AIzaSyD-y9MrdrHxyCUpNuqsrPYKfWSdYw2zo-E&playlistId=PLogA9DP2_vSekxHP73PXaKD6nbOK56CJw&part=snippet")
         .then(response => response.json())
         .then((data) => {
             setCovidVideos(data.items);
@@ -38,11 +42,23 @@ function VideoPreview(props) {
         }
 
         videos.forEach((video) => {
+
+            let videoId = ''
+            if (video.snippet.playlistId && (video.snippet.playlistId === "PLogA9DP2_vSekxHP73PXaKD6nbOK56CJw")) {
+                videoId = video.snippet.resourceId.videoId
+            } else {
+                videoId = video.id.videoId
+            }
+  
+
             list.push(
                 <div key={video.etag}>
-                    <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
+                    <iframe title={video.snippet.title} id="player" type="text/html" width="640" height="390"
+                        src={`https://www.youtube.com/embed/${videoId}`}
+                    frameBorder="0"></iframe>
                     <div>
-                        <h1>{video.snippet.title}</h1>
+                         {/* multiple replaces can probably be refactored */}
+                        <h1>{(video.snippet.title).replace("&amp;", "&").replace("&#39;", "'")}</h1>
                         <p>{video.snippet.description}</p>
                     </div>
                     <br/>
@@ -50,8 +66,6 @@ function VideoPreview(props) {
             );
         });
     
-
-
   return (
     <>{list}</>
   )
